eba0918d8a8f8773fa75a91e275bc2345fb044cf
p-br <p.brown@tutanota.com>
2022-04-04 16:34:29 +0200

    cleaned

--- scripts/single-item.js
+++ scripts/single-item.js
@@ -3,15 +3,19 @@ const nameElement = document.querySelector("#name");
 const genresElement = document.querySelector("#genres");
 const summaryElement = document.querySelector("#summary");
 const posterURL = document.querySelector("#poster");
+const runTimeElement = document.querySelector("#runtime");
+const broadcastElement = document.querySelector("#broadcast");
+
 
 // get string from url and keep only 3digit ID (substring)
 const searchedItem = document.location.search.substring(9);
 
 // create DOM elements
 const insertElements = (element) => {
-    nameElement.textContent = `${element.name}`;
-    genresElement.textContent = `${element.genres}`;
-    summaryElement.innerHTML = `${element.summary}`;
+    nameElement.textContent = element.name;
+    genresElement.textContent = element.genres;
+    summaryElement.innerHTML = element.summary;
+    runTimeElement.textContent = `${element.runtime} minutes`;
     posterURL.innerHTML = posterURL.src=element.image.medium;
     return;
 };
@@ -26,3 +30,6 @@ fetch(`https://api.tvmaze.com/shows/${searchedItem}`).then((response) => {
         document.title = `${body.name}`;
     });
 });
+
+
+moviediscription.innerHTML = moviediscription.src=element.image.medium;
