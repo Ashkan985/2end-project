1307757cd843cf040faf61a61ac4dc364a84e2a7
p-br <p.brown@tutanota.com>
2022-04-04 15:57:57 +0200

    updated version
    all tested

--- scripts/single-item.js
+++ scripts/single-item.js
@@ -1,28 +1,28 @@
-// document title
-document.title = "test";
-
-
 //  declarations
 const nameElement = document.querySelector("#name");
 const genresElement = document.querySelector("#genres");
 const summaryElement = document.querySelector("#summary");
+const posterURL = document.querySelector("#poster");
+
+// get string from url and keep only 3digit ID (substring)
+const searchedItem = document.location.search.substring(9);
 
 // create DOM elements
 const insertElements = (element) => {
     nameElement.textContent = `${element.name}`;
     genresElement.textContent = `${element.genres}`;
     summaryElement.innerHTML = `${element.summary}`;
+    posterURL.innerHTML = posterURL.src=element.image.medium;
     return;
 };
 
 // fetch from TVMaze
-
-fetch("https://api.tvmaze.com/singlesearch/shows?q=batman").then((response) => {
+fetch(`https://api.tvmaze.com/shows/${searchedItem}`).then((response) => {
     response.json().then((body) => {
         insertElements(body);
-    });
-})
+        // console.log(body.name)
 
-//  TO DO
-// document.title to change dynamically
-// in fetch, replace "batman" with the search item from main page js file
+        // page title
+        document.title = `${body.name}`;
+    });
+});
