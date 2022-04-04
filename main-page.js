05ce9cdba7371903fff54a2209b5c49e3e69498d
Ashkan <ashkan.tavakoli.moghadam@gmail.com>
2022-04-04 16:23:32 +0200

    new update

--- main-page.js
+++ main-page.js
@@ -1,24 +1,27 @@
-
-
-
-const Btn = document.querySelector(".btn")
-const input = document.querySelector(".users-input")
-
-
+const Btn = document.querySelector(".btn");
+const input = document.querySelector(".users-input");
 
 Btn.addEventListener("click", () => {
-    
-    fetch(`https://api.tvmaze.com/search/shows?q=${input.value}`).then(
-      (res) => {
-        res.json().then((newversion) => {
+  fetch(`https://api.tvmaze.com/search/shows?q=${input.value}`).then((res) => {
+    res.json().then((newversion) => {
+      newversion.forEach((element) => {
+        const result = document.createElement("div");
+        document.querySelector(".suggested-movies").appendChild(result);
 
-            newversion.forEach(element => {
-                
-           const result = document.createElement('')
-          console.log(newversion);
-        //  `<div>${newversion.id}</div><div>${newversion.url}</div><div>${newversion.summary}</div>`;
-        });
-      }
-    );
+        const movieName = document.createElement("h3");
+        document.querySelector("div").appendChild(movieName);
+        movieName.textContent = element.show.name;
+
+        const movieDiscription = document.createElement("p");
+        document.querySelector("div").appendChild(movieDiscription);
+        movieDiscription.innerHTML = element.show.summary;
+
+        const movieImg = document.createElement("img");
+        document.querySelector("div").appendChild(movieImg);
+        movieDiscription.innerHTML.src = element.show.image.medium;
+      });
+      console.log(newversion);
+    });
   });
- });
+});
+
