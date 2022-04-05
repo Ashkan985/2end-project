940711c1ba5e276492f5cdac7046a1314c905a26
p-br <p.brown@tutanota.com>
2022-04-05 10:59:35 +0200

    updated links

--- assets/scripts/single-item.js
+++ assets/scripts/single-item.js
@@ -8,25 +8,28 @@ const broadcastElement = document.querySelector("#broadcast");
 
 
 // get string from url and keep only 3digit ID (substring)
-const searchedItem = document.location.search.substring(9);
+// const searchedItem = document.location.search.substring(9);
+// const searchedItem = document.location.search.slice(9);
+const searchedItem = document.location.search.split('=')[1];
 
 // create DOM elements
 const insertElements = (element) => {
     nameElement.textContent = element.name;
-    genresElement.textContent = element.genres;
+    genresElement.textContent = element.genres.join(" - ");
     summaryElement.innerHTML = element.summary;
     runTimeElement.textContent = `${element.runtime} minutes`;
+    broadcastElement.textContent = element.premiered;
     posterURL.src = element.image.medium;
     return;
 };
 
 // fetch from TVMaze
 fetch(`https://api.tvmaze.com/shows/${searchedItem}`).then((response) => {
-    response.json().then((body) => {
-        insertElements(body);
-        // console.log(body.name)
+    response.json().then((data) => {
+        insertElements(data);
+        // console.log(data.name)
 
         // page title
-        document.title = `${body.name}`;
+        document.title = `${data.name}`;
     });
 });
